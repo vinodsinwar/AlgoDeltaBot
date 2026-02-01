ac75b8e32ff64c44402aea2003d46321a3b45e32
vinodsinwar <vinsoft.devlab@gmail.com>
2026-02-01 16:17:41 +0530

    style: Minimalist Alert Header (Sequence - Title | Signs Legend)

--- scan.js
+++ scan.js
@@ -130,8 +130,8 @@ async function runScan() {
         const seq = now.getUTCHours().toString().padStart(2, '0') +
             now.getUTCMinutes().toString().padStart(2, '0');
 
-        let msg = `## ${seq} 🧪 **Profit Rate Scan** 🧪 TH: > ±0.35%\n`;
-        msg += `_Legends: 🔴 Pay 🟢 | 🟢 Pay 🔴_\n\n`;
+        let msg = `## ${seq} - Profit Rate Scan 🧪\n`;
+        msg += `TH: > ±0.35% | Legends: - Pay + | + Pay -\n\n`;
 
         msg += "```\n";
         // Header: Compact columns (Tightened)
